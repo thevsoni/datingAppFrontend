fd9ebe0d043152220251e50bb0411629d0350453
Vishal Soni <vs332837@gmail.com>
2023-05-08 19:58:39 +0530

    updated

--- src/APIs/Axios.js
+++ src/APIs/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Axios = axios.create({
     // baseURL: "http://localhost:5000/",
-    baseURL: "https://thevsoni-datingappbackend.onrender.com",
+    baseURL: "https://thevsoni-datingappbackend.onrender.com/",
 })
 
 export default Axios;
