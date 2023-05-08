a767a809a23c16c20c7eb9e04239bd68bd947b48
Vishal Soni <vs332837@gmail.com>
2023-05-08 19:36:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -20,14 +20,23 @@ function App() {
         <Navbar />
         <Routes>
 
-          <Route path='/' exact Component={LandingScreen} />
+          {/* <Route path='/' exact Component={LandingScreen} />
           <Route path='/home' exact Component={HomeScreen} />
           <Route path='/signup' exact Component={SignupScreen} />
           <Route path='/login' exact Component={LoginScreen} />
           <Route path='/profile' exact Component={ProfileScreen} />
           <Route path='/friendsPending' exact Component={FriendsPending} />
           <Route path='/friends' exact Component={Friends} />
-          <Route path='/talkingRoom/:providerId' exact Component={TalkingScreen} />
+          <Route path='/talkingRoom/:providerId' exact Component={TalkingScreen} /> */}
+
+          <Route path='/' element={<LandingScreen />} />
+          <Route path='/home' element={<HomeScreen />} />
+          <Route path='/signup' element={<SignupScreen />} />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route path='/profile' element={<ProfileScreen />} />
+          <Route path='/friendsPending' element={<FriendsPending />} />
+          <Route path='/friends' element={<Friends />} />
+          <Route path='/talkingRoom/:providerId' element={<TalkingScreen />} />
         </Routes>
       </BrowserRouter>
     </div>
